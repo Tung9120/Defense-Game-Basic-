cfd49c89257978d80928299446ce0c11240cd7a8
Tung Vu <vutung959595@gmail.com>
2022-07-29 09:48:17 +0700

    ShopManager Script added

---
+++ Assets/Tung9120/Defense_Game_Basic/Scripts/DataStruct.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Tung9120.DefenseBasic
+{
+    [Serializable]
+    public class ShopItem
+    {
+        public Player playerPrefabs;
+        public int price;
+        public Sprite previewImg;
+    }
+}
