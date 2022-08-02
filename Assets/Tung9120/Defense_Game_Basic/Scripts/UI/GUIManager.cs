b931c38e84674136d39604ce0935f59dd08cb80a
Tung Vu <vutung959595@gmail.com>
2022-08-02 15:29:59 +0700

    Create GUI manager script

---
+++ Assets/Tung9120/Defense_Game_Basic/Scripts/UI/GUIManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Tung9120.DefenseBasic
+{
+    public class GUIManager : MonoBehaviour
+    {
+        public GameObject homeGUI;
+        public GameObject gameGUI;
+        public Dialog gameoverDialog;
+        public Text mainCoinTxt;
+        public Text gameplayCoinTxt;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        public void ShowGameGUI(bool isShow)
+        {
+            if (gameGUI)
+                gameGUI.SetActive(isShow);
+
+            if (homeGUI)
+                homeGUI.SetActive(!isShow);
+        }
+
+        public void UpdateMainCoins()
+        {
+            if (mainCoinTxt)
+                mainCoinTxt.text = Pref.coins.ToString();
+        }
+
+        public void UpdateGameplayCoins()
+        {
+            if (gameplayCoinTxt)
+                gameplayCoinTxt.text = Pref.coins.ToString();
+        }
+    }
+}
