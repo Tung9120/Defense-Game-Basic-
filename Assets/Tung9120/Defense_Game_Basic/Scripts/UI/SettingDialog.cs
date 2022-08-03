98d5ce7d8fa2b90bb78a2b591ef2aaf1d2bfb6e3
Tung Vu <vutung959595@gmail.com>
2022-08-03 11:20:45 +0700

    Script for ShopDialog Part 01

--- Assets/Tung9120/Defense_Game_Basic/Scripts/UI/SettingDialog.cs
+++ Assets/Tung9120/Defense_Game_Basic/Scripts/UI/SettingDialog.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Tung9120.DefenseBasic
 {
--- Assets/Tung9120/Defense_Game_Basic/Scripts/UI/ShopDialog.cs
+++ Assets/Tung9120/Defense_Game_Basic/Scripts/UI/ShopDialog.cs
@@ -4,18 +4,64 @@ using UnityEngine;
 
 namespace Tung9120.DefenseBasic
 {
-    public class ShopDialog : Dialog
+    public class ShopDialog : Dialog, IComponentChecking
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        public Transform gridRoot;
+        public ShopItemUI itemUIPrefab;
+        public ShopManager m_shopMng;
+        public GameManager m_gm;
 
+        public override void Show(bool isShow)
+        {
+            base.Show(isShow);
+
+            m_shopMng = FindObjectOfType<ShopManager>();
+            m_gm = FindObjectOfType<GameManager>();
+
+            UpdateUI();
         }
 
-        // Update is called once per frame
-        void Update()
+        public bool IsComponentsNull()
         {
+            return m_shopMng == null && m_gm == null || gridRoot == null;
+        }
 
+        private void UpdateUI()
+        {
+            if (IsComponentsNull()) return;
+
+            ClearChilds();
+
+            var items = m_shopMng.items;
+
+            if (items == null || items.Length <= 0) return;
+
+            for(int i = 0; i < items.Length; i++)
+            {
+                int idx = i;
+
+                var item = items[idx];
+
+                var itemUIClone = Instantiate(itemUIPrefab, Vector3.zero, Quaternion.identity);
+
+                itemUIClone.transform.SetParent(gridRoot);
+                itemUIClone.transform.localScale = Vector3.one;
+                itemUIClone.transform.localPosition = Vector3.zero;
+                itemUIClone.UpdateUI(item, idx);
+            }
+        }
+
+        public void ClearChilds()
+        {
+            if (gridRoot == null || gridRoot.childCount <= 0) return;
+
+            for(int i = 0; i < gridRoot.childCount; i++)
+            {
+                var child = gridRoot.GetChild(i);
+
+                if (child)
+                    Destroy(child.gameObject);
+            }
         }
     }
 }
